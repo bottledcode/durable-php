cc7cb599a19550771d1e99a88b1635ace5fd779a
Robert Landers <landers.robert@gmail.com>
2023-05-14 23:45:26 +0200

    start building workers

--- src/Worker.php
+++ src/Worker.php
@@ -2,4 +2,17 @@
 
 namespace Bottledcode\DurablePhp;
 
+use parallel\Channel;
+
 require_once __DIR__ . '/../vendor/autoload.php';
+
+abstract class Worker
+{
+    public static function errorHandler(): void
+    {
+    }
+
+    abstract public function run(Channel $commander);
+}
+
+set_error_handler(Worker::errorHandler(...));
