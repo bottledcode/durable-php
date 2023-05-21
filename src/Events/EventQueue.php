decd76d75bb4f5d049c6deba74e7ffde80c50956
Robert Landers <landers.robert@gmail.com>
2023-05-21 22:58:01 +0200

    start figuring this out?

---
+++ src/Events/EventQueue.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bottledcode\DurablePhp\Events;
+
+use Bottledcode\DurablePhp\State\OrchestrationInstance;
+
+class EventQueue
+{
+    private array $queue = [];
+    private array $order = [];
+    private bool $dirty = false;
+
+    public function insert(Event $event): void
+    {
+        $this->queue[$this->getKey($event)][] = $event;
+        $this->dirty = true;
+    }
+
+    private function getKey(Event $event): string
+    {
+        if ($event instanceof HasInstanceInterface) {
+            return "{$event->getInstance()->instanceId}:{$event->getInstance()->executionId}";
+        }
+
+        return $event->eventId;
+    }
+
+    public function setHeat(OrchestrationInstance ...$instance): void
+    {
+
+    }
+
+    public function getNext(): Event|null
+    {
+    }
+}
--- src/Run.php
+++ src/Run.php
@@ -2,11 +2,24 @@
 
 namespace Bottledcode\DurablePhp;
 
+use Amp\Future;
+use Amp\NullCancellation;
 use Amp\Parallel\Context\DefaultContextFactory;
 use Amp\Parallel\Worker\ContextWorkerFactory;
 use Amp\Parallel\Worker\ContextWorkerPool;
+use Amp\Parallel\Worker\Execution;
+use Amp\Parallel\Worker\WorkerPool;
+use Amp\Sync\ChannelException;
+use Amp\Sync\LocalKeyedSemaphore;
+use Amp\Sync\RateLimitingSemaphore;
+use Amp\Sync\SemaphoreMutex;
 use Bottledcode\DurablePhp\Contexts\LoggingContextFactory;
+use Bottledcode\DurablePhp\Events\Event;
+use Bottledcode\DurablePhp\Events\HasInstanceInterface;
+use Ramsey\Collection\DoubleEndedQueue;
+use Withinboredom\Time\Seconds;
 
+use function Amp\async;
 use function Amp\Parallel\Worker\workerPool;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -17,10 +30,59 @@ class Run
     {
     }
 
+    private function doWork(WorkerPool $pool, Event $event): void {
+        /**
+         * @var array<array-key, Execution> $map
+         */
+        static $map = [];
+        static $queue = new \SplQueue();
+
+        $mapKey = $event->eventId;
+        if($event instanceof HasInstanceInterface) {
+            $mapKey = "{$event->getInstance()->instanceId}:{$event->getInstance()->executionId}";
+        }
+
+        if(isset($map[$mapKey])) {
+            try {
+                $channel = $map[$mapKey]->getChannel();
+                $channel->send($event);
+            } catch(ChannelException) {
+                // now try reading back any events that were lost
+
+                unset($map[$mapKey]);
+            }
+        }
+
+        $map[$mapKey] = $pool->submit(new EventDispatcherTask($this->config, $event));
+    }
+
     public function __invoke(): void
     {
+        $queue = new \SplQueue();
+        $map = [];
+
         $pool = $this->createPool($this->config);
         $this->createPartition();
+        foreach ($this->replay() as $event) {
+            if($event instanceof HasInstanceInterface) {
+                if(isset($map[$event->getInstance()->instanceId])) {
+                    $queue->enqueue($event);
+                    continue;
+                }
+            }
+            $w = $pool->submit(new EventDispatcherTask($this->config, $event));
+            $w->getChannel()->send('purple');
+            $result = $w->getChannel()->receive(new NullCancellation());
+        }
+        Logger::log('Replay completed');
+        foreach ($this->consumeEvents() as $event) {
+            if($event === null) {
+                // todo: housekeeping
+                continue;
+            }
+
+            $pool->submit(new EventDispatcherTask($this->config, $event));
+        }
     }
 
     private function createPool(Config $config): ContextWorkerPool
@@ -49,11 +111,72 @@ class Run
     {
         return $this->config->partitionKeyPrefix . $this->config->currentPartition;
     }
+
+    private function replay(): \Generator
+    {
+        $lastEventId = '0-0';
+        replay:
+        $events = $this->readNext($lastEventId, 50, null);
+        Logger::log('replaying ' . count($events) . ' events');
+        if (empty($events)) {
+            return;
+        }
+        foreach ($events as $eventId => ['event' => $event]) {
+            if ($event === null) {
+                continue;
+            }
+            /**
+             * @var Event $devent
+             */
+            $devent = igbinary_unserialize($event);
+            $lastEventId = $devent->eventId = $eventId;
+
+            Logger::log('replaying %s event', get_class($devent));
+            yield $devent;
+        }
+        goto replay;
+    }
+
+    private function readNext(string $from, int $count, Seconds|null $timeout): Future
+    {
+        return async(function() {
+            [$this->getPartitionKey() => $events] = $this->redis->xReadGroup(
+                'consumer_group',
+                'consumer',
+                [$this->getPartitionKey() => $from],
+                $count,
+                $timeout?->inMilliseconds()
+            );
+            return $events;
+        });
+    }
+
+    private function consumeEvents(): \Generator
+    {
+        while (true) {
+            $events = $this->readNext('>', 50, new Seconds(30));
+            if (empty($events)) {
+                continue;
+            }
+            foreach ($events as $eventId => ['event' => $event]) {
+                if ($event === null) {
+                    continue;
+                }
+                /**
+                 * @var Event $devent
+                 */
+                $devent = igbinary_unserialize($event);
+                $devent->eventId = $eventId;
+                yield $devent;
+            }
+        }
+    }
 }
 
 (static function ($argv) {
     $config = Config::fromArgs($argv);
-    $runner = new Run($config);
+    $runner = new Run($config, RedisReaderTask::connect($config));
+    $runner();
 })(
     $argv
 );
