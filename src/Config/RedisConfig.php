996e36923d2760cfe7b0f88f8250c40e89e4af39
Rob Landers <landers.robert@gmail.com>
2023-05-28 21:35:37 +0200

    start implementing rethinkdb

--- src/Config/RedisConfig.php
+++ src/Config/RedisConfig.php
@@ -21,41 +21,13 @@
  * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-namespace Bottledcode\DurablePhp;
+namespace Bottledcode\DurablePhp\Config;
 
-use Amp\Parallel\Worker\Environment;
-use Bottledcode\DurablePhp\Config\Config;
-use Redis;
-use RedisCluster;
-
-abstract class Worker implements \Amp\Parallel\Worker\Task
+readonly class RedisConfig
 {
-	private $timesCollectedGarbage = 0;
-
-	public function __construct(protected Config $config)
-	{
-	}
-
-	public static function errorHandler(...$props): void
-	{
-		echo 'Error: ' . json_encode($props) . PHP_EOL;
-		die(1);
-	}
-
-	abstract public function run(Environment $environment): void;
-
-	protected function heartbeat(Redis|RedisCluster $redis, string $name): void
-	{
-		$redis->set(sprintf('partition_%d:%s:heartbeat', $this->config->currentPartition, $name), time());
-	}
-
-	protected function collectGarbage(): bool
-	{
-		if ($this->timesCollectedGarbage++ % 100 === 0) {
-			gc_collect_cycles();
-			return true;
-		}
-
-		return false;
+	public function __construct(
+		public string $host = 'localhost',
+		public int $port = 6379,
+	) {
 	}
 }
