cc4c20251cc3b69067e6581827b5eb7e7f94eaa6
Robert Landers <landers.robert@gmail.com>
2023-05-28 14:00:38 +0200

    start working on a proper dev env

---
+++ devspace_start.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set +e  # Continue on errors
+
+# Start Apache
+/etc/init.d/apache2 restart
+
+# Ensure file permissions
+chown -R www-data:www-data .
+
+export COMPOSER_ALLOW_SUPERUSER=1
+if [ -f "composer.json" ]; then
+   echo "Installing PHP dependencies"
+   composer install \
+    --ignore-platform-reqs \
+    --no-interaction \
+    --no-plugins \
+    --no-scripts \
+    --prefer-dist
+fi
+
+export NODE_ENV=development
+if [ -f "yarn.lock" ]; then
+   echo "Installing Yarn Dependencies"
+   yarn
+else 
+   if [ -f "package.json" ]; then
+      echo "Installing NPM Dependencies"
+      npm install
+   fi
+fi
+
+COLOR_BLUE="\033[0;94m"
+COLOR_GREEN="\033[0;92m"
+COLOR_RESET="\033[0m"
+
+# Print useful output for user
+echo -e "${COLOR_BLUE}
+     %########%      
+     %###########%       ____                 _____                      
+         %#########%    |  _ \   ___ __   __ / ___/  ____    ____   ____ ___ 
+         %#########%    | | | | / _ \\\\\ \ / / \___ \ |  _ \  / _  | / __// _ \\
+     %#############%    | |_| |(  __/ \ V /  ____) )| |_) )( (_| |( (__(  __/
+     %#############%    |____/  \___|  \_/   \____/ |  __/  \__,_| \___\\\\\___|
+ %###############%                                  |_|
+ %###########%${COLOR_RESET}
+
+
+Welcome to your development container!
+
+This is how you can work with it:
+- Files will be synchronized between your local machine and this container
+- Some ports will be forwarded, so you can access this container via localhost
+"
+
+# Set terminal prompt
+export PS1="\[${COLOR_BLUE}\]devspace\[${COLOR_RESET}\] ./\W \[${COLOR_BLUE}\]\\$\[${COLOR_RESET}\] "
+if [ -z "$BASH" ]; then export PS1="$ "; fi
+
+# Include project's bin/ folder in PATH
+export PATH="./bin:$PATH"
+
+# Open shell
+bash --norc
