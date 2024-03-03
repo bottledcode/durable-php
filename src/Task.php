9968de037b88d435c43ec7d41bad54cb49abb033
Robert Landers <landers.robert@gmail.com>
2024-03-03 13:32:22 +0100

    handle activities, maybe

--- src/Task.php
+++ src/Task.php
@@ -26,25 +26,32 @@ namespace Bottledcode\DurablePhp;
 // we are in the context of this project (testing/developing)
 use Basis\Nats\AmpClient;
 use Basis\Nats\Configuration;
+use Bottledcode\DurablePhp\Events\Event;
 use Bottledcode\DurablePhp\Events\EventDescription;
+use Bottledcode\DurablePhp\Events\HasInnerEventInterface;
+use Bottledcode\DurablePhp\Events\PoisonPill;
+use Bottledcode\DurablePhp\State\AbstractHistory;
 use Bottledcode\DurablePhp\State\ActivityHistory;
 use Bottledcode\DurablePhp\State\Serializer;
 use Bottledcode\DurablePhp\State\StateInterface;
 use Bottledcode\DurablePhp\Transmutation\Router;
 use Monolog\Level;
+use Ramsey\Uuid\Uuid;
 
-if (file_exists(__DIR__ . "/../vendor/autoload.php")) {
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require_once __DIR__ . '/../vendor/autoload.php';
 }
 
 // we are in the context of a project
-if (file_exists(__DIR__ . "/../../../vendor/autoload.php")) {
-    require_once __DIR__ . "/../../../vendor/autoload.php";
+if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../../vendor/autoload.php';
 }
 
 class Task
 {
     use Router;
+
+    private array $batch = [];
 
     public function __construct(private DurableLogger $logger, private AmpClient $nats) {}
 
@@ -56,57 +63,78 @@ class Task
             case 'event':
                 $subject = $route[1] ?? null;
                 if ($subject === null) {
-                    $this->logger->critical('subject must be set');
-                    http_response_code(404);
-                    die();
+                    $this->emitError(404, 'subject must be set');
                 }
                 [$stream, $type, $_] = explode('.', $subject);
                 if ($stream !== getenv('NATS_STREAM')) {
-                    $this->logger->critical('NATS_STREAM must match event stream');
-                    http_response_code(404);
-                    die();
+                    $this->emitError(404, 'NATS_STREAM must match event stream');
                 }
                 try {
-                    $event = EventDescription::fromJson(file_get_contents("php://input"));
+                    $event = EventDescription::fromJson(file_get_contents('php://input'));
                 } catch (\JsonException $e) {
-                    $this->logger->critical('json decoding error', ['exception' => $e]);
-                    http_response_code(400);
-                    die();
+                    $this->emitError(400, 'json decoding error', ['exception' => $e]);
                 }
 
                 switch ($type) {
                     case 'activities':
                         $activity = $event->destination;
-                        if (!$activity->isActivityId()) {
-                            $this->logger->critical('Invalid activity id');
-                            http_response_code(400);
-                            die();
+                        if (! $activity->isActivityId()) {
+                            $this->emitError(400, 'Invalid activity id');
                         }
 
                         try {
-                            ['version' => $expectedVersion, 'state' => $state] =
-                                    loadState($subject, $this->nats) ?? new ActivityHistory($activity, $this->logger);
+                            /**
+                             * @var AbstractHistory $state
+                             * @var int $expectedVersion
+                             */
+                            ['version' => $expectedVersion, 'state' => $state] = $this->loadState($subject, $this->nats);
                         } catch (\JsonException $e) {
-                            $this->logger->critical('json decoding error', ['exception' => $e]);
-                            http_response_code(400);
-                            die();
+                            $this->emitError(400, 'json decoding error', ['exception' => $e]);
                         }
 
-                        $events = applyEvent($event, $state);
+                        if ($state === null) {
+                            $state = new ActivityHistory($activity, $this->logger);
+                        }
 
-                        die();
+                        if ($state->hasAppliedEvent($event->event)) {
+                            $this->emitError(200, 'event already applied', ['event' => $event]);
+                        }
+
+                        try {
+                            foreach ($this->transmutate($event->innerEvent, $state, $event->event) as $eventOrCallable) {
+                                if ($eventOrCallable instanceof Event) {
+                                    if ($eventOrCallable instanceof PoisonPill) {
+                                        break;
+                                    }
+                                    $this->fire($eventOrCallable);
+                                } elseif ($eventOrCallable instanceof \Closure) {
+                                    $eventOrCallable($this);
+                                }
+                            }
+                        } catch (\Throwable $exception) {
+                            $this->emitError(
+                                500,
+                                'Failed to process',
+                                ['event' => $event, 'exception' => $exception]
+                            );
+                        }
+
+                        $state->resetState();
+                        $this->commit($state, $subject, $expectedVersion);
+
+                        exit();
                     case 'orchestrations':
                         echo "ERROR: orchestrations not implemented yet\n";
                         http_response_code(500);
-                        die();
+                        exit();
                     case 'entities':
                         echo "ERROR: entities not implemented yet\n";
                         http_response_code(500);
-                        die();
+                        exit();
                     default:
                         echo "ERROR: unknown route type \"{$type}\"\n";
                         http_response_code(404);
-                        die();
+                        exit();
                 }
             case 'activity':
             case 'entity':
@@ -117,20 +145,60 @@ class Task
         http_response_code(404);
     }
 
-    private function loadState(string $subject, AmpClient $client): array|null
+    private function emitError(int $code, string $message, array $context = []): never
+    {
+        $logger = match (true) {
+            $code >= 500 => $this->logger->critical(...),
+            $code >= 400 => $this->logger->warning(...),
+            default => $this->logger->info(...),
+        };
+        $logger($message, $context);
+        http_response_code($code);
+        exit();
+    }
+
+    private function loadState(string $subject, AmpClient $client): array
     {
         $bucket = $client->getApi()->getBucket($subject);
-        $state = $bucket->get('state');
-        if(empty($state)) {
-            return null;
+        $entry = $bucket->getEntry('state');
+        if ($entry === null) {
+            return [0, null];
         }
-        ['version' => $version, 'state' => $state] = json_decode($state, true, 512, JSON_THROW_ON_ERROR);
+        $state = json_decode($entry->value, true, 512, JSON_THROW_ON_ERROR);
+        $version = $entry->revision;
 
         return ['version' => $version, 'state' => Serializer::deserialize($state, StateInterface::class)];
     }
+
+    public function fire(Event ...$events): array
+    {
+        $ids = [];
+        foreach ($events as $event) {
+            $this->logger->debug('Batching', ['event' => $event]);
+            $parent = $event;
+            if (empty($event->eventId)) {
+                $id = Uuid::uuid7();
+                while ($event instanceof HasInnerEventInterface) {
+                    $event->eventId = $id;
+                    $event = $event->getInnerEvent();
+                }
+            }
+            $this->batch[] = $parent;
+            $ids[] = $parent->eventId;
+        }
+
+        return $ids;
+    }
+
+    private function commit(AbstractHistory $state, string $subject, int $version): void
+    {
+        $bucket = $this->nats->getApi()->getBucket($subject);
+        $sequence = $bucket->update('state', json_encode(Serializer::serialize($state), JSON_THROW_ON_ERROR), $version);
+        $this->logger->alert('Pushed state', ['version' => $version, 'new seq' => $sequence]);
+    }
 }
 
-$logger = new DurableLogger(level: match(getenv('LOG_LEVEL') ?: 'INFO') {
+$logger = new DurableLogger(level: match (getenv('LOG_LEVEL') ?: 'INFO') {
     'DEBUG' => Level::Debug,
     'INFO' => Level::Info,
     'ERROR' => Level::Error,
