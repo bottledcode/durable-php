2212d142724870e05d114728c48b7cf6acccde2e
Robert Landers <landers.robert@gmail.com>
2024-03-23 18:50:46 +0100

    implement the basic resolvers

--- src/Gateway/Graph/index.php
+++ src/Gateway/Graph/index.php
@@ -25,22 +25,84 @@ namespace Bottledcode\DurablePhp\Gateway\Graph;
 
 // todo: load user specific schema based on code
 
+use Bottledcode\DurablePhp\DurableClient;
+use Bottledcode\DurablePhp\State\EntityId;
+use Bottledcode\DurablePhp\State\OrchestrationInstance;
+use Bottledcode\DurablePhp\State\Serializer;
+use GraphQL\Error\DebugFlag;
 use GraphQL\Language\AST\TypeDefinitionNode;
 use GraphQL\Server\ServerConfig;
 use GraphQL\Server\StandardServer;
+use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Utils\BuildSchema;
 
 require_once __DIR__ . '/../../Glue/autoload.php';
 
+header('Content-Type: text/plain');
+
+$client = DurableClient::get();
+$client->withAuth(str_replace('Bearer ', '', $_SERVER['HTTP_AUTHORIZATION']));
+
 $decorator = function (array $typeConfig, TypeDefinitionNode $typeDefinitionNode): array {
+    $name = $typeConfig['name'];
+
+    switch($name) {
+        case 'Query':
+            $typeConfig['resolveField'] = function ($value, array $args, DurableClient $context, ResolveInfo $resolveInfo): mixed {
+                switch($resolveInfo->fieldName) {
+                    case 'orchestration':
+                        $id = new OrchestrationInstance($args['id']['instance'], $args['id']['execution']);
+                        if($args['waitForCompletion'] ?? false) {
+                            return Serializer::serialize($context->waitForCompletion($id));
+                        }
+
+                        return Serializer::serialize($context->getStatus($id));
+                    case 'entity':
+                        $id = new EntityId($args['id']['name'], $args['id']['id']);
+                        return Serializer::serialize($context->getEntitySnapshot($id));
+                }
+
+                return 'error';
+            };
+            break;
+        case 'Mutation':
+            $typeConfig['resolveField'] = function ($value, array $args, DurableClient $context, ResolveInfo $resolveInfo): mixed {
+                switch($resolveInfo->fieldName) {
+                    case 'StartOrchestration':
+                        $id = $context->startNew($args['name'], $args['input'], $args['id'] ?? null);
+                        return [
+                            'instance' => $id->instanceId,
+                            'execution' => $id->executionId,
+                        ];
+                    case 'RaiseOrchestrationEvent':
+                        $id = new OrchestrationInstance($args['id']['instance'], $args['id']['execution']);
+                        $arguments = array_map(static fn($x, $i) => ['key' => $i, ...$x], $args['arguments'], range(0, count($args['arguments']) - 1));
+                        $arguments = array_column($arguments, 'value', 'key');
+                        $context->raiseEvent($id, $args['signal'], $arguments);
+                        return [];
+                    case 'SendEntitySignal':
+                        $id = new EntityId($args['id']['name'], $args['id']['id']);
+                        $arguments = array_map(static fn($x, $i) => ['key' => $i, ...$x], $args['arguments'], range(0, count($args['arguments']) - 1));
+                        $arguments = array_column($arguments, 'value', 'key');
+                        $context->signalEntity($id, $args['signal'], $arguments);
+                        return [];
+                }
+
+                return 'error';
+            };
+            break;
+        case 'Orchestration':
+            break;
+    }
+
     return $typeConfig;
 };
 
 // todo: caching
 $contents = file_get_contents(__DIR__ . '/generic-schema.graphql');
 $schema = BuildSchema::build($contents, $decorator);
-$config = new ServerConfig();
-$config->setSchema($schema);
+
+$config = ServerConfig::create()->setSchema($schema)->setContext($client)->setDebugFlag(DebugFlag::INCLUDE_DEBUG_MESSAGE);
 
 $server = new StandardServer($config);
 $server->handleRequest();
