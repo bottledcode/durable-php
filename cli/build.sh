f76a7b4977af77114e50ffdc78c4f648980f90a5
Robert Landers <landers.robert@gmail.com>
2024-04-12 02:28:33 +0200

    just name it something simple

--- cli/build.sh
+++ cli/build.sh
@@ -24,9 +24,9 @@
 
 set -o errexit
 
-if ! type "git" > /dev/null; then
-    echo "The \"git\" command must be installed."
-    exit 1
+if ! type "git" >/dev/null; then
+  echo "The \"git\" command must be installed."
+  exit 1
 fi
 
 cd ../dist/static-php-cli
@@ -35,57 +35,57 @@ arch="$(uname -m)"
 os="$(uname -s | tr '[:upper:]' '[:lower:]')"
 md5binary="md5sum"
 if [ "${os}" = "darwin" ]; then
-    os="mac"
-    md5binary="md5 -q"
+  os="mac"
+  md5binary="md5 -q"
 fi
 
 if [ -z "${PHP_EXTENSIONS}" ]; then
-    export PHP_EXTENSIONS="apcu,bcmath,bz2,calendar,ctype,curl,dom,exif,fileinfo,filter,gmp,gd,iconv,igbinary,mbregex,mbstring,opcache,openssl,pcntl,phar,posix,readline,simplexml,sockets,sodium,sysvsem,tokenizer,uuid,uv,xml,xmlreader,xmlwriter,zip,zlib"
+  export PHP_EXTENSIONS="apcu,bcmath,bz2,calendar,ctype,curl,dom,exif,fileinfo,filter,gmp,gd,iconv,igbinary,mbregex,mbstring,opcache,openssl,pcntl,phar,posix,readline,simplexml,sockets,sodium,sysvsem,tokenizer,uuid,uv,xml,xmlreader,xmlwriter,zip,zlib"
 fi
 
 if [ -z "${PHP_EXTENSION_LIBS}" ]; then
-    export PHP_EXTENSION_LIBS="bzip2,freetype,libavif,libjpeg,libwebp,libzip"
+  export PHP_EXTENSION_LIBS="bzip2,freetype,libavif,libjpeg,libwebp,libzip"
 fi
 
 if [ -z "${PHP_VERSION}" ]; then
-    export PHP_VERSION="8.3"
+  export PHP_VERSION="8.3"
 fi
 
 if [ -z "${FRANKENPHP_VERSION}" ]; then
-    FRANKENPHP_VERSION="dev"
-    export FRANKENPHP_VERSION
+  FRANKENPHP_VERSION="dev"
+  export FRANKENPHP_VERSION
 elif [ -d ".git/" ]; then
-    CURRENT_REF="$(git rev-parse --abbrev-ref HEAD)"
-    export CURRENT_REF
+  CURRENT_REF="$(git rev-parse --abbrev-ref HEAD)"
+  export CURRENT_REF
 
-    if echo "${FRANKENPHP_VERSION}" | grep -F -q "."; then
-        # Tag
+  if echo "${FRANKENPHP_VERSION}" | grep -F -q "."; then
+    # Tag
 
-        # Trim "v" prefix if any
-        FRANKENPHP_VERSION=${FRANKENPHP_VERSION#v}
-        export FRANKENPHP_VERSION
+    # Trim "v" prefix if any
+    FRANKENPHP_VERSION=${FRANKENPHP_VERSION#v}
+    export FRANKENPHP_VERSION
 
-        git checkout "v${FRANKENPHP_VERSION}"
-    else
-        git checkout "${FRANKENPHP_VERSION}"
-    fi
+    git checkout "v${FRANKENPHP_VERSION}"
+  else
+    git checkout "${FRANKENPHP_VERSION}"
+  fi
 fi
 
-bin="dphp-${os}-${arch}"
+bin="dphp"
 
 if [ -n "${CLEAN}" ]; then
-    rm -Rf dist/
-    go clean -cache
+  rm -Rf dist/
+  go clean -cache
 fi
 
 CGO_CFLAGS="-O2 -DFRANKENPHP_VERSION=${FRANKENPHP_VERSION} -I${PWD}/buildroot/include/ $(./buildroot/bin/php-config --includes | sed s\#-I/\#-I"${PWD}"/buildroot/\#g)"
 if [ -n "${DEBUG_SYMBOLS}" ]; then
-    CGO_CFLAGS="-g ${CGO_CFLAGS}"
+  CGO_CFLAGS="-g ${CGO_CFLAGS}"
 fi
 export CGO_CFLAGS
 
 if [ "${os}" = "mac" ]; then
-    export CGO_LDFLAGS="-framework CoreFoundation -framework SystemConfiguration"
+  export CGO_LDFLAGS="-framework CoreFoundation -framework SystemConfiguration"
 fi
 
 CGO_LDFLAGS="${CGO_LDFLAGS}  ${PWD}/buildroot/lib/libbrotlicommon.a ${PWD}/buildroot/lib/libbrotlienc.a ${PWD}/buildroot/lib/libbrotlidec.a $(./buildroot/bin/php-config --ldflags) $(./buildroot/bin/php-config --libs) -lstdc++ -lbrotlidec -lssl -lcrypto -lbrotlienc -lbrotlicommon"
@@ -97,22 +97,22 @@ export LIBPHP_VERSION
 cd ../../cli
 
 VERSION="$(git describe --tags --always)"
-if  git status --porcelain | grep -q "cli"; then
+if git status --porcelain | grep -q "cli"; then
   VERSION="$VERSION-dirty"
 fi
 
 # Embed PHP app, if any
 if [ -n "${EMBED}" ] && [ -d "${EMBED}" ]; then
-    tar -cf app.tar -C "${EMBED}" .
-    ${md5binary} app.tar > app_checksum.txt
+  tar -cf app.tar -C "${EMBED}" .
+  ${md5binary} app.tar >app_checksum.txt
 fi
 
 if [ "${os}" = "linux" ]; then
-    extraExtldflags="-Wl,-z,stack-size=0x80000"
+  extraExtldflags="-Wl,-z,stack-size=0x80000"
 fi
 
 if [ -z "${DEBUG_SYMBOLS}" ]; then
-    extraLdflags="-w -s -race"
+  extraLdflags="-w -s -race"
 fi
 
 env
@@ -121,14 +121,14 @@ go get durable_php
 go build -buildmode=pie -tags "cgo netgo nats osusergo static_build" -ldflags "-linkmode=external -extldflags '-static-pie ${extraExtldflags}' ${extraLdflags} -X 'github.com/caddyserver/caddy/v2.CustomVersion=FrankenPHP ${FRANKENPHP_VERSION} PHP ${LIBPHP_VERSION} go_durable_php' -X 'main.version=$VERSION'" -o "dist/${bin}" durable_php
 
 if [ -d "${EMBED}" ]; then
-    truncate -s 0 app.tar
-    truncate -s 0 app_checksum.txt
+  truncate -s 0 app.tar
+  truncate -s 0 app_checksum.txt
 fi
 
 if [ -z "${NO_COMPRESS}" ]; then
-  if type "upx" > /dev/null; then
-      #upx --best "dist/${bin}"
-      echo "would compress"
+  if type "upx" >/dev/null; then
+    #upx --best "dist/${bin}"
+    echo "would compress"
   fi
 fi
 
