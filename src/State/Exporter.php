4ace08d30a2b55390444b86310174816d8576c0e
Robert Landers <landers.robert@gmail.com>
2025-07-28 23:52:53 +0200

    output logging
    Signed-off-by: Robert Landers <landers.robert@gmail.com>

--- src/State/Exporter.php
+++ src/State/Exporter.php
@@ -31,6 +31,7 @@ class Exporter extends DictionaryExporter
     public function importValue(Deserializer $deserializer, Field $field, mixed $source): mixed
     {
         $reflectedRecord = new ReflectionClass($field->phpType);
+        error_log(print_r($source, true));
         if ($source === null || $source['root'] === null) {
             return null;
         }
